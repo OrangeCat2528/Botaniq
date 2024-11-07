230d53dbfc17cd5c65db71cd1e7fd71a9b4b5f4c
OrangeCat. <farrelnaufalpramono@gmail.com>
2024-11-07 11:33:08 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -5,7 +5,7 @@ require_once './layout/header.php';
 require_once './layout/sidebar.php';
 ?>
 
-<div class="flex flex-col items-center bg-gray-50 min-h-screen pt-2">
+<div class="flex flex-col items-center min-h-screen pt-2">
     <!-- Profile Image Section -->
     <div id="avatar-container" class="p-2 bg-white rounded-xl shadow-sm flex justify-center items-center">
         <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Profile Image" class="w-24 h-24 rounded-full border-2 border-gray-300">
