a2b12665327b4c199c5c7c341a1559eb9c8e3e60
OrangeCat. <farrelnaufalpramono@gmail.com>
2024-11-07 11:21:22 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -5,12 +5,22 @@ require_once './layout/header.php';
 require_once './layout/sidebar.php';
 ?>
 
-  <!-- Bagian yang Harus anda Isi -->
+<div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <!-- Profile Image -->
+    <div class="mb-4">
+        <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Profile Image" class="w-32 h-32 rounded-full border-4 border-gray-300">
+    </div>
 
-  <?php
-  require_once './layout/bottom.php';
-  ?>
+    <!-- Buttons -->
+    <div class="flex space-x-4">
+        <a href="logout.php" class="bg-gray-500 text-white font-semibold py-2 px-4 rounded-full hover:bg-gray-600 transition duration-200">LOG OUT</a>
+        <a href="change-password.php" class="bg-gray-500 text-white font-semibold py-2 px-4 rounded-full hover:bg-gray-600 transition duration-200">CHANGE PASS</a>
+    </div>
+</div>
+
+<?php
+require_once './layout/bottom.php';
+?>
 
 </body>
-
 </html>
