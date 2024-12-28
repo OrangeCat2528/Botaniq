d83cc4205a5295c8d71de860984921272cf0736d
OrangeCat. <farrelnaufalpramono@gmail.com>
2024-12-28 20:30:42 +0700

    restore

--- helper/encryption.php
+++ helper/encryption.php
@@ -1,12 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-define('ENCRYPTION_KEY', $_ENV['ENCRYPT_DATA']);
+define('ENCRYPTION_KEY', 'DB8u25EUQfuvf4yp');
 
 function encryptToken($data)
 {
