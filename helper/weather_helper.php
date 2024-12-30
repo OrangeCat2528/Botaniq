40d6d96b0e79dd0a34062a91e36fe47d88f6f541
OrangeCat. <farrelnaufalpramono@gmail.com>
2024-12-30 10:04:24 +0700

    add load environment

--- helper/weather_helper.php
+++ helper/weather_helper.php
@@ -1,6 +1,11 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 $allowedOrigins = [
    'http://botaniq.test',
    'https://botaniq.cogarden.app',
