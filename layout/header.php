a77f5faaf1eea8fb14e45ca6a8d737645df97842
OrangeCat. <farrelnaufalpramono@gmail.com>
2025-01-02 19:13:19 +0700

    add notification icon

--- layout/header.php
+++ layout/header.php
@@ -32,8 +32,9 @@ $headerTitles = [
 $headerTitle = $headerTitles[$page] ?? "Botaniq SuperApp";
 ?>
 
-<div class="text-center py-5 mb-5 shadow-md text-gray-600 bg-white">
+<div class="text-center py-5 mb-5 shadow-md text-gray-600 bg-white relative">
     <button id="sidebar-toggle" class="absolute left-5 items-center fa fa-user-circle text-gray text-3xl"></button>
     <img src="/assets/img/superapp-login-logo-only.png" alt="Botaniq SuperApp Logo" class="inline-block w-8 h-8" id="header-icon">
     <span class="font-extrabold text-lg ml-1"><?= htmlspecialchars($headerTitle) ?></span>
+    <button id="notification-icon" class="absolute right-5 items-center fa fa-bell text-gray text-3xl"></button>
 </div>
